3ecccf04daaf6009d9b701cbe8e2ff7755611be9
jc mulit <vaultdweller123@gmail.com>
2011-11-28 10:53:23 +0800

    added validation on webpage and template - stable

--- admin/update_template2.php
+++ admin/update_template2.php
@@ -1,24 +1,35 @@
 <?php 
 
+require_once("connect.php");
+
+
 $id = $_POST['id'];
 $name = mysql_real_escape_string($_POST['template_name']);
 $content = mysql_real_escape_string($_POST['content']);
 
 
+if($name){
 
+	$sql = mysql_query("UPDATE template SET name='".$name."', content='".$content."' WHERE id='".$id."'");
 
+	if($sql){
+		echo "<script type='text/javascript'>alert('Update Successul');</script>";
+		echo "<script type='text/javascript'>window.location='/admin/update_template.php?id=".$id."'</script>";
+	}else{
+		echo "<script type='text/javascript'>alert('Fail');</script>";
+		echo "<script type='text/javascript'>window.location='/admin/update_template.php?id=".$id."'</script>";
+	}
 
-require_once("connect.php");
-
-$sql = mysql_query("UPDATE template SET name='".$name."', content='".$content."' WHERE id='".$id."'");
-
-if($sql){
-echo "<script type='text/javascript'>alert('Update Successul');</script>";
-echo "<script type='text/javascript'>window.location='/admin/update_template.php?id=".$id."'</script>";
 }else{
-echo "<script type='text/javascript'>alert('Fail');</script>";
-echo "<script type='text/javascript'>window.location='/admin/update_template.php?id=".$id."'</script>";
+
+	echo "<script type='text/javascript'>alert('No tempate name entered! reverting back to orinal content');</script>";
+	echo "<script type='text/javascript'>window.location='/admin/update_template.php?id=".$id."'</script>";
+
 }
+
+
+
+
 
 
 ?>
--- admin/update_webpage2.php
+++ admin/update_webpage2.php
@@ -1,25 +1,36 @@
 <?php 
 
+require_once("connect.php");
+
 $id = $_POST['id'];
 $template = $_POST['template'];
 $name = mysql_real_escape_string($_POST['page_name']);
 $content = mysql_real_escape_string($_POST['content']);
 
 
+if($name){
+
+	$sql = mysql_query("UPDATE webpage SET name='".$name."', content='".$content."', temp_id='".$template."' WHERE id='".$id."'");
+
+	if($sql){
+		echo "<script type='text/javascript'>alert('Update Successul');</script>";
+		echo "<script type='text/javascript'>window.location='/admin/update_webpage.php?id=".$id."'</script>";
+	}else{
+		echo "<script type='text/javascript'>alert('Fail');</script>";
+		echo "<script type='text/javascript'>window.location='/admin/update_webpage.php?id=".$id."'</script>";
+	}
 
 
-
-require_once("connect.php");
-
-$sql = mysql_query("UPDATE webpage SET name='".$name."', content='".$content."', temp_id='".$template."' WHERE id='".$id."'");
-
-if($sql){
-echo "<script type='text/javascript'>alert('Update Successul');</script>";
-echo "<script type='text/javascript'>window.location='/admin/update_webpage.php?id=".$id."'</script>";
 }else{
-echo "<script type='text/javascript'>alert('Fail');</script>";
-echo "<script type='text/javascript'>window.location='/admin/update_webpage.php?id=".$id."'</script>";
+
+	echo "<script type='text/javascript'>alert('No page name entered! reverting back to orinal content');</script>";
+	echo "<script type='text/javascript'>window.location='/admin/update_webpage.php?id=".$id."'</script>";
+
 }
+
+
+
+
 
 
 ?>
