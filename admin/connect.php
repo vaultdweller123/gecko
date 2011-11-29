61665401186eef4bf0c497ed5efbefe1e1b5a073
jc mulit <vaultdweller123@gmail.com>
2011-11-29 09:24:08 +0800

    wewe

--- admin/connect.php
+++ admin/connect.php
@@ -3,7 +3,7 @@
 
 include_once('class/config.php');
 
-// connect
+// connect...
 $conf = new Gconfig();
 mysql_connect($conf->dbhost,$conf->dbuser,$conf->dbpass);
 mysql_select_db($conf->database);
