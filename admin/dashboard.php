6c7ad0ed7e196c543073b55753777642b2c31e04
Jc Mulit <vaultdweller123@gmail.com>
2011-11-30 20:29:16 +0800

    new skin for dashboard

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -3,24 +3,47 @@
 session_start();
 if(isset($_SESSION['id'])){
 ?>
-<html>
-<head>
-</head>
-<body>
-<h1>Dashboard</h1>
-<p>
-<a href="/admin/webpage.php">web pages</a><br />
-</p>
-<p>
-<a href="/admin/template.php">templates</a>
-<p>
-<p>
-<a href="/admin/menu.php">dynamic menu</a>
-</p>
-<p>
-<a href="/admin/logout.php">logout</a><br />
-</p>
-</body>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+	<head>
+		<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+		<title>Steal My Admin</title>
+		<link rel="stylesheet" href="css/960.css" type="text/css" media="screen" charset="utf-8" />
+		<link rel="stylesheet" href="css/template.css" type="text/css" media="screen" charset="utf-8" />
+		<link rel="stylesheet" href="css/colour.css" type="text/css" media="screen" charset="utf-8" />
+		<style type="text/css">
+		#navigation a{
+		text-decoration:none;
+		}
+		</style>
+	</head>
+	<body>
+
+		<h1 id="head">Gecko</h1>
+
+		<ul id="navigation">
+			<li><span><a href="/admin/webpage.php">Web Pages</a></span></li>
+			<li><a href="/admin/template.php">Templates</a></li>
+			<li><a href="/admin/menu.php">Dynamic menu</a></li>
+		</ul>
+		
+		<div id="content" class="container_16 clearfix">
+			<div class="grid_11" style="width:auto!important;" >
+				<h2>About</h2>
+				<p>Welcome to our 1st ever kick ass CMS</p>
+				<h3>Credits</h3>
+				<p>I would like to thank TGG dota boys for the great support</p>
+			</div>
+			
+		</div>
+		
+		<div id="foot">
+					<a href="/admin/logout.php">Logout</a>
+		</div>
+
+		
+	</body>
 </html>
 <?php
 //prevent URL direct access - end
